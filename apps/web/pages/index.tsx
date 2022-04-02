1dee38169bed7754d3805b7a9e1c8df26a60705b
PranjalAgni <pranjal.agnihotri6@gmail.com>
2022-04-02 22:28:38 +0530

    :tada: added google fonts

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,9 +1,9 @@
-import { Button } from "ui";
+import { Button } from 'ui';
 
 export default function Web() {
   return (
     <div>
-      <h1>Web</h1>
+      <h1>Hello there 💚🚀👋</h1>
       <Button />
     </div>
   );
