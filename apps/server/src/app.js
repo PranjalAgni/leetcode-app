cc4f5164869386218e6b091b5685a10ea11bed8f
PranjalAgni <pranjal.agnihotri6@gmail.com>
2022-04-13 23:47:53 +0530

    :computer: added cors on server side

--- apps/server/src/app.js
+++ apps/server/src/app.js
@@ -1,12 +1,16 @@
 const Fastify = require('fastify');
 const connectDB = require('./db/');
 const registerAPI = require('./routes/');
-
+const fastifyCors = require('fastify-cors');
 const initalizeApp = (opts = {}) => {
   connectDB();
   const fastify = Fastify(opts);
 
   fastify.register(registerAPI);
+
+  fastify.register(fastifyCors, {
+    origin: ['http://localhost:3000'],
+  });
 
   return fastify;
 };
