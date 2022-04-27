892a30a8703f09b75319a15af0f6beb7e0cb8d13
PranjalAgni <pranjal.agnihotri6@gmail.com>
2022-04-27 23:55:45 +0530

    :sunflower: made columns sticky + added overflow to grid

--- apps/web/components/Table/Table.tsx
+++ apps/web/components/Table/Table.tsx
@@ -1,3 +1,5 @@
+import styles from './Table.module.css';
+
 type TableProps = {
   data: { name: string; url: string; solvedAt: string }[];
   columns: string[];
@@ -5,29 +7,37 @@ type TableProps = {
 
 export default function Table({ data, columns }: TableProps) {
   return (
-    <table>
-      <thead>
-        <tr>
-          {columns.map((column, idx) => (
-            <th key={idx} scope="col">
-              {column}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((row, idx) => {
-          const date = new Date(row.solvedAt);
-          const formattedSolvedAtDate = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
-          return (
-            <tr key={idx}>
-              <td>{row.name}</td>
-              <td>{row.url}</td>
-              <td>{formattedSolvedAtDate}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <div className={styles.tableWrapper}>
+      <table>
+        <thead>
+          <tr>
+            {columns.map((column, idx) => (
+              <th key={idx} scope="col">
+                {column}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((row, idx) => {
+            const date = new Date(row.solvedAt);
+            const formattedSolvedAtDate = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
+            return (
+              <tr key={idx}>
+                <td>{row.name}</td>
+                <td>
+                  {
+                    <a target="_blank" rel="noreferrer" href={row.url}>
+                      {row.url}
+                    </a>
+                  }
+                </td>
+                <td>{formattedSolvedAtDate}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 }
