d9f2363b4081668cdc14ad1f9efc1559a423eeff
PranjalAgni <pranjal.agnihotri6@gmail.com>
2023-05-24 23:41:14 +0530

    :mountain_snow: used timeago to find how recently problem was solved

--- apps/web/pages/dashboard.tsx
+++ apps/web/pages/dashboard.tsx
@@ -1,10 +1,14 @@
 import { useQuery } from 'react-query';
+import TimeAgo from 'javascript-time-ago';
+import en from 'javascript-time-ago/locale/en';
 import Layout from '../components/Layout/Layout';
 import Table from '../components/Table/Table';
 import { fetchProblems } from '../services/problems';
 import { sortByDate } from '../utils/date';
 
+TimeAgo.addDefaultLocale(en);
 export default function Dashboard() {
+  const timeago = new TimeAgo('en-US');
   const problems = useQuery('problems', fetchProblems);
   console.log('React query ', problems);
   if (problems.isLoading) return <button aria-busy="true">Loading ⏰</button>;
@@ -13,6 +17,12 @@ export default function Dashboard() {
   }
 
   const solvedLeetCodeProblemsInSortedFashion = sortByDate(problems.data.data);
+  solvedLeetCodeProblemsInSortedFashion.forEach((leetcodeProblem) => {
+    const [day, month, year] = leetcodeProblem.solvedAt.split('/');
+    const solvedAtMs = new Date(`${month}/${day}/${year}`).getTime();
+    const solvedAgo = timeago.format(solvedAtMs);
+    console.log(`${leetcodeProblem.solvedAt} ---> ${solvedAgo}`);
+  });
 
   const columns = Object.keys(solvedLeetCodeProblemsInSortedFashion[0]).map(
     (column) => column.toUpperCase()
